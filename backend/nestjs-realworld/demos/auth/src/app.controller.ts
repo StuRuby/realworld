af58ab5bf01a8ebbc71f2a931acb17c48f05be33
937931249 <937931249@qq.com>
2019-09-07 20:15:30 +0800

    Update auth demo

--- backend/nestjs-realworld/demos/auth/src/app.controller.ts
+++ backend/nestjs-realworld/demos/auth/src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, UseGuards, Request } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @UseGuards(AuthGuard('local'))
+  @Post('login')
+  async login(@Request() req) {
+    return req.user;
   }
 }
