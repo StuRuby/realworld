ffacb94054350605725825d5328a7d829bb9bc40
937931249 <937931249@qq.com>
2019-09-07 18:09:47 +0800

    Add auth module

---
+++ backend/nestjs-realworld/demos/auth/src/users/users.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@nestjs/common';
+
+export type User = any;
+
+@Injectable()
+export class UsersService {
+    private readonly users: User[];
+
+    constructor() {
+        this.users = [
+            {
+                id: 1,
+                name: 'jon',
+                password: 'jon123',
+            },
+            {
+                id: 2,
+                name: 'jane',
+                password: 'jane123',
+            },
+            {
+                id: 3,
+                name: 'tom',
+                password: 'tom123',
+            },
+            {
+                id: 4,
+                name: 'alice',
+                password: 'alice123',
+            },
+        ];
+    }
+
+    async findOne(name: string): Promise<User | undefined> {
+        return this.users.find(user => user.name === name);
+    }
+}
